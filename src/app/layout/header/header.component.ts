b422f359ea12c3808e25c57244f2dfea271e2b8e
farkhatkoshbaev2002@mail.ru <ImPaLa1997Fara20027%7%7%>
2025-04-11 01:58:21 +0700

    apload 6

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [ MatButtonModule],
+  imports: [ MatButtonModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
